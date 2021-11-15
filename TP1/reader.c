0e62b7026a2ba4c7efe5a2cd515b331c74d6f9e2
Martin Rios <jrios@fi.uba.ar>
2021-11-15 11:45:18 -0300

    Se agrega la primer versión funcional.

--- TP1/reader.c
+++ TP1/reader.c
@@ -8,31 +8,38 @@
 #include <unistd.h>
 #include <stdint.h>
 
-#define FIFO_NAME "myfifo"
-#define BUFFER_SIZE 300
+#define FIFO_NAME           "myfifo"
+#define SIGN_FILE_NAME      "Sign.txt"
+#define DATA_FILE_NAME      "Log.txt"
+#define BUFFER_SIZE         300
+#define ERROR_STR_SIZE      64
+
+#define DATA_PREFIX         "DATA:"
+#define SIGN_PREFIX         "SIGN:"
 
 int main(void)
 {
-	uint8_t inputBuffer[BUFFER_SIZE];
+	char inputBuffer[BUFFER_SIZE], errorStr[ERROR_STR_SIZE];
 	int32_t bytesRead, returnCode, fd;
+    FILE *pDF, *pSF;
     
     /* Create named fifo. -1 means already exists so no action if already exists */
     if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) < -1  )
     {
-        printf("Error creating named fifo: %d\n", returnCode);
+        printf("Error al crear la cola nombrada: %d\n", returnCode);
         exit(1);
     }
     
     /* Open named fifo. Blocks until other process opens it */
-	printf("waiting for writers...\n");
+	printf("Esperando por escritores...\n");
 	if ( (fd = open(FIFO_NAME, O_RDONLY) ) < 0 )
     {
-        printf("Error opening named fifo file: %d\n", fd);
+        printf("Error abriendo el archivo de cola nombrada: %d\n", fd);
         exit(1);
     }
     
     /* open syscalls returned without error -> other process attached to named fifo */
-	printf("got a writer\n");
+	printf("Se obtuvo un escritor.\n");
 
     /* Loop until read syscall returns a value <= 0 */
 	do
@@ -46,6 +53,32 @@ int main(void)
 		{
 			inputBuffer[bytesRead] = '\0';
 			printf("reader: read %d bytes: \"%s\"\n", bytesRead, inputBuffer);
+
+            if(!strncmp(inputBuffer, DATA_PREFIX, strlen(DATA_PREFIX)))
+            {            
+                pDF = fopen( DATA_FILE_NAME, "a" );
+                if ( pDF == NULL )
+                {
+                    strcpy(errorStr, "Error al abrir el archivo: ");
+                    perror(strcat(errorStr, DATA_FILE_NAME));
+                    exit(1);
+                } 
+                fprintf(pDF, "%s\n", inputBuffer);
+                fclose(pDF);
+            }
+
+            else if(!strncmp(inputBuffer, SIGN_PREFIX, strlen(SIGN_PREFIX)))
+            {            
+                pSF = fopen( SIGN_FILE_NAME, "a" );
+                if ( pSF == NULL )
+                {
+                    strcpy(errorStr, "Error al abrir el archivo: ");
+                    perror(strcat(errorStr, SIGN_FILE_NAME));
+                    exit(1);
+                } 
+                fprintf(pSF, "%s\n", inputBuffer);
+                fclose(pSF);
+            }            
 		}
 	}
 	while (bytesRead > 0);
--- TP1/writer.c
+++ TP1/writer.c
@@ -7,41 +7,81 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <signal.h>
 
-#define FIFO_NAME "myfifo"
-#define BUFFER_SIZE 300
+#define FIFO_NAME           "myfifo"
+#define BUFFER_SIZE         300
+#define DATA_PREFIX         "DATA:"
+#define BUFFER_OFFSET       strlen(DATA_PREFIX)
+#define SIGN_PREFIX         "SIGN:"
+#define SIGN_MSG            "Signal recibida!\n"
+
+volatile sig_atomic_t got_sigusr = 0;
+
+void sigusr_handler(int sig)
+{
+    write(1, SIGN_MSG, strlen(SIGN_MSG));
+    got_sigusr = sig;
+}
 
 int main(void)
 {
-   
-    char outputBuffer[BUFFER_SIZE];
+    char outputBuffer[BUFFER_SIZE] = {DATA_PREFIX};
 	uint32_t bytesWrote;
 	int32_t returnCode, fd;
+
+    struct sigaction sa;
+
+    sa.sa_handler = sigusr_handler;
+    sa.sa_flags = SA_SIGINFO;//SA_RESTART; //
+    //sygemptyset(&sa.sa_mask);
+
+    if(sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
+    }
+
+    if(sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
+    }    
 
     /* Create named fifo. -1 means already exists so no action if already exists */
     if ( (returnCode = mknod(FIFO_NAME, S_IFIFO | 0666, 0) ) < -1 )
     {
-        printf("Error creating named fifo: %d\n", returnCode);
+        printf("Error al crear la cola nombrada: %d\n", returnCode);
         exit(1);
     }
 
     /* Open named fifo. Blocks until other process opens it */
-	printf("waiting for readers...\n");
+	printf("Esperando por lectores...\n");
 	if ( (fd = open(FIFO_NAME, O_WRONLY) ) < 0 )
     {
-        printf("Error opening named fifo file: %d\n", fd);
+        printf("Error abriendo el archivo de cola nombrada: %d\n", fd);
         exit(1);
     }
     
     /* open syscalls returned without error -> other process attached to named fifo */
-	printf("got a reader--type some stuff\n");
+	printf("Se ha conectado un lector, escribe algo...\n");
 
     /* Loop forever */
 	while (1)
 	{
         /* Get some text from console */
-		fgets(outputBuffer, BUFFER_SIZE, stdin);
-        
+        snprintf(outputBuffer, BUFFER_SIZE, "%s", DATA_PREFIX);
+        if( fgets(outputBuffer+BUFFER_OFFSET, BUFFER_SIZE, stdin) == NULL)
+        {
+            perror("fgets");
+        }
+
+        if(got_sigusr)
+        {            
+            sprintf(outputBuffer, "%s%d\n", SIGN_PREFIX, got_sigusr-SIGUSR1+1);
+            got_sigusr = 0;
+        }      
+
         /* Write buffer to named fifo. Strlen - 1 to avoid sending \n char */
 		if ((bytesWrote = write(fd, outputBuffer, strlen(outputBuffer)-1)) == -1)
         {
@@ -49,7 +89,7 @@ int main(void)
         }
         else
         {
-			printf("writer: wrote %d bytes\n", bytesWrote);
+			printf("Writer: wrote %d bytes, %s", bytesWrote, outputBuffer);
         }
 	}
 	return 0;
